7a4c29f766ac8098bb10bc673f70287e08ce95e4
andrewheo <andrewheo1225@gmail.com>
2021-05-23 20:43:47 -0400

    add VIP Cleaners information

---
+++ src/components/Cards.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
+
+function Cards() {
+    return (
+        <div className = 'cards'>
+            <h1>Check Out These Epic Destinations!</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem
+                        src = "images/img-9.jpg"
+                        text = "Explore the waterfalls deep inside the Amazon Jungle"
+                        label = 'Adventure'
+                        path = '/services'
+                        />
+                        <CardItem
+                        src = "images/img-2.jpg"
+                        text = "Travel through the islands of Bali"
+                        label = 'Luxury'
+                        path = '/services'
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem
+                        src = "images/img-3.jpg"
+                        text = "Set sail in the Atlantic Ocean"
+                        label = 'Mystery'
+                        path = '/services'
+                        />
+                        <CardItem
+                        src = "images/img-4.jpg"
+                        text = "Experience Football on top of the Mountains"
+                        label = 'Adventure'
+                        path = '/products'
+                        />
+                        <CardItem
+                        src = "images/img-8.jpg"
+                        text = "Ride through the Sahara Desert"
+                        label = 'Adrenaline'
+                        path = '/sign-up'
+                        />
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Cards
---
+++ src/components/HeroSection.js
@@ -0,0 +1,33 @@
+import { Button } from './Button';
+import React from "react";
+import '../App.css';
+import './HeroSection.css';
+ 
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <h1>Adventure Awaits</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Get Started
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Watch Trailer <i className='far fa-play-circle'/>
+        </Button>
+
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
